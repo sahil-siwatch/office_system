4d25f862f0ac07ade5bbf1c10c8344df554cb2af
sahil-siwatch <sahilsiwatch07@gmail.com.com>
2020-02-20 18:03:58 +0530

    led_blinked

---
+++ STM8L/Projects/try/main.c
@@ -0,0 +1,120 @@
+/**
+  ******************************************************************************
+  * @file    GPIO/GPIO_Toggle/main.c
+  * @author  MCD Application Team
+  * @version V1.5.2
+  * @date    30-September-2014
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm8l15x.h"
+#include "stm8l15x_gpio.h"
+//#include "iostm8l152c6.h"
+
+
+/** @addtogroup STM8L15x_StdPeriph_Examples
+  * @{
+  */
+
+/** @addtogroup GPIO_Toggle
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* define the GPIO port and pins connected to Leds mounted on STM8L152X-EVAL board */
+
+#define LED_GPIO_PORT  GPIOE
+#define LED_GPIO_PINS  GPIO_Pin_7
+
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+void Delay (uint16_t nCount);
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+void main(void)
+{
+  PE_ODR = 0;
+  PE_DDR_DDR7 = 1;
+  PE_CR1_C17 = 1;
+  PE_CR2_C27 = 1;
+  
+  
+  while(1)
+  {
+    PE_ODR_bit.ODR7 = 1;
+    Delay(90000000);
+    PE_ODR_bit.ODR7 = 0;
+    Delay(90000000);
+  }
+ 
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nCount: specifies the delay time length.
+  * @retval None
+  */
+void Delay(uint16_t nCount)
+{
+  /* Decrement nCount value */
+  while (nCount != 0)
+  {
+    nCount--;
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* Infinite loop */
+  while (1)
+  {}
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ projectd/led/main.c
@@ -0,0 +1,29 @@
+#include "iostm8l152c6.h"
+
+void Delay(long nCount)
+{
+  /* Decrement nCount value */
+  while (nCount != 0)
+  {
+    nCount--;
+  }
+}
+
+
+int main( void )
+{
+  PE_ODR = 0;
+  PE_DDR_DDR7 = 1;
+  PE_CR1_C17 = 1;
+  PE_CR2_C27 = 1;
+  
+  
+  while(1)
+  {
+    PE_ODR_bit.ODR7 = 1;
+    Delay(100000);
+    PE_ODR_bit.ODR7 = 0;
+    Delay(100000);
+  }
+ 
+}
