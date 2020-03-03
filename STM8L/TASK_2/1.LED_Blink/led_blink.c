ec39659ca0c39c882c6fcea247cd36e57759c697
sahil-siwatch <sahilsiwatch07@gmail.com.com>
2020-03-03 18:07:23 +0530

    source code of project added

--- STM8L/TASK_2/1.LED_Blink/led_blink.c
+++ STM8L/TASK_2/1.LED_Blink/led_blink.c
@@ -1,105 +1,84 @@
-/**
-  ******************************************************************************
-  * @file    Project/STM8L15x_StdPeriph_Template/main.c
-  * @author  MCD Application Team
-  * @version V1.6.1
-  * @date    30-September-2014
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "stm8l15x.h"
+#include "stm8l15x_it.h"
 
-/** @addtogroup STM8L15x_StdPeriph_Template
-  * @{
-  */
+void Delay(long);
+void configure_gpio(void);
+void configure_interrupt(void);
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
+unsigned int buttonPress = 0, tryBit;
 
-/* Private functions ---------------------------------------------------------*/
-
-/**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
-
-
-#define LED_GPIO_PORT  GPIOE
-#define LED_GPIO_PINS  GPIO_Pin_7
-
-void Delay(long nCount);
-
-void main(void)
+INTERRUPT_HANDLER(EXTI1_IRQHandler, 9)
 {
-  /* Initialize LEDs mounted on STM8L152X-EVAL board */
-  GPIO_Init(LED_GPIO_PORT, LED_GPIO_PINS, GPIO_Mode_Out_PP_Low_Fast);
-
-  while (1)
+  if((GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == FALSE)&&(buttonPress == 0))
   {
-    /* Toggle LEDs LD1..LD4 */
-    GPIO_WriteBit(LED_GPIO_PORT, LED_GPIO_PINS, 1);
-    Delay(100000);
-    GPIO_WriteBit(LED_GPIO_PORT, LED_GPIO_PINS, 0);
-    Delay(100000);
+    while((GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == FALSE)&&(buttonPress == 0));
+    tryBit = 3;
+    buttonPress = 1;
   }
+  else if((GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == FALSE)&&(buttonPress == 1))
+  {
+    while((GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == FALSE)&&(buttonPress == 1));
+    tryBit = 4;
+    buttonPress = 0;
+  }
+  
+  EXTI_ClearITPendingBit(EXTI_IT_Pin1);
+}
+
+
+int main()
+{
+  int count;
+	configure_gpio();
+	configure_interrupt();
+	while(1)
+	{
+          {
+                 GPIO_WriteBit(GPIOE, GPIO_Pin_7, 1);
+                 Delay(100000);
+                 GPIO_WriteBit(GPIOE, GPIO_Pin_7, 0);
+                 Delay(100000);             
+          }
+          if(tryBit == 3)
+          {
+            halt();
+          }
+	}
+}
+
+void configure_gpio()
+{
+  GPIO_DeInit(GPIOE);
+  GPIO_DeInit(GPIOC);
+  
+  GPIO_Init(GPIOE, GPIO_Pin_7, GPIO_Mode_Out_PP_Low_Fast);
+  GPIO_Init(GPIOC, GPIO_Pin_1, GPIO_Mode_In_PU_IT);
+/*
+	PE_ODR = 0;
+	PE_DDR_DDR7 = 1;
+	PC_DDR_DDR1 = 0;
+
+  PE_CR1_C17 = 1;  // output as push pull
+  PE_CR2_C27 = 1; // output speed upto 10MHz
+  
+  PC_CR1_C11 = 1;  // input with pull-up
+  PC_CR2_C21 = 1; // external interrupt enabled
+
+  EXTI_SetPinSensitivity(EXTI_Pin_TypeDef EXTI_Pin, EXTI_Trigger_TypeDef EXTI_Trigger);
+*/
+}
+
+void configure_interrupt()
+{
+	EXTI_SetPinSensitivity(EXTI_Pin_1, EXTI_Trigger_Falling);
+	enableInterrupts();
 }
 
 void Delay(long nCount)
 {
-  /* Decrement nCount value */
+
   while (nCount != 0)
   {
     nCount--;
   }
 }
-
-
-#ifdef  USE_FULL_ASSERT
-
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *   where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
-}
-#endif
-
-/**
-  * @}
-  */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- STM8L/TASK_2/Low Power/Low_Power.c
+++ STM8L/TASK_2/Low Power/Low_Power.c
@@ -1,88 +1,84 @@
 #include "stm8l15x.h"
+#include "stm8l15x_it.h"
 
-bool state = FALSE;
-void GPIO_setup(void);
-//void EXTI_setup(void);
-void clock_setup(void);
 void Delay(long);
-void main(void)
+void configure_gpio(void);
+void configure_interrupt(void);
+
+unsigned int buttonPress = 0, tryBit;
+
+INTERRUPT_HANDLER(EXTI1_IRQHandler, 9)
 {
-       unsigned char i = 0;
-       GPIO_setup();
-       //EXTI_setup();
-       clock_setup();
-       for(i = 0; i <= 6; i++)
-       {
-              GPIO_WriteReverse(GPIOE, GPIO_Pin_7);
-              Delay(100000);
-       }
-       wfi();
-       while (TRUE)
-       {
-              for(i = 0; i < 4; i++)
-              {
-                     GPIO_WriteReverse(GPIOE, GPIO_Pin_7);
-                     Delay(100000);
-              }
-              for(i = 0; i < 4; i++)
-              {
-                     GPIO_WriteReverse(GPIOE, GPIO_Pin_7);
-                     Delay(100000);
-              }
-              halt();
-       }
+  if((GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == FALSE)&&(buttonPress == 0))
+  {
+    while((GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == FALSE)&&(buttonPress == 0));
+    tryBit = 3;
+    buttonPress = 1;
+  }
+  else if((GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == FALSE)&&(buttonPress == 1))
+  {
+    while((GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == FALSE)&&(buttonPress == 1));
+    tryBit = 4;
+    buttonPress = 0;
+  }
+  
+  EXTI_ClearITPendingBit(EXTI_IT_Pin1);
 }
+
+
+int main()
+{
+  int count;
+  configure_gpio();
+  configure_interrupt();
+  while(1)
+  {
+          {
+                 GPIO_WriteBit(GPIOE, GPIO_Pin_7, 1);
+                 Delay(100000);
+                 GPIO_WriteBit(GPIOE, GPIO_Pin_7, 0);
+                 Delay(100000);             
+          }
+          if(tryBit == 3)
+          {
+            halt();
+          }
+  }
+}
+
+void configure_gpio()
+{
+  GPIO_DeInit(GPIOE);
+  GPIO_DeInit(GPIOC);
+  
+  GPIO_Init(GPIOE, GPIO_Pin_7, GPIO_Mode_Out_PP_Low_Fast);
+  GPIO_Init(GPIOC, GPIO_Pin_1, GPIO_Mode_In_PU_IT);
+/*
+  PE_ODR = 0;
+  PE_DDR_DDR7 = 1;
+  PC_DDR_DDR1 = 0;
+
+  PE_CR1_C17 = 1;  // output as push pull
+  PE_CR2_C27 = 1; // output speed upto 10MHz
+  
+  PC_CR1_C11 = 1;  // input with pull-up
+  PC_CR2_C21 = 1; // external interrupt enabled
+
+  EXTI_SetPinSensitivity(EXTI_Pin_TypeDef EXTI_Pin, EXTI_Trigger_TypeDef EXTI_Trigger);
+*/
+}
+
+void configure_interrupt()
+{
+  EXTI_SetPinSensitivity(EXTI_Pin_1, EXTI_Trigger_Falling);
+  enableInterrupts();
+}
+
 void Delay(long nCount)
 {
+
   while (nCount != 0)
   {
     nCount--;
   }
-}
-void GPIO_setup(void)
-{
-       GPIO_DeInit(GPIOC);   
-       GPIO_Init(GPIOC, GPIO_Pin_1, GPIO_Mode_In_PU_IT);
-       GPIO_DeInit(GPIOE);
-       GPIO_Init(GPIOE, GPIO_Pin_7, GPIO_Mode_Out_PP_Low_Fast);
-}
-/*
-void EXTI_setup(void)
-{
-       ITC_DeInit();
-       ITC_SetSoftwarePriority(ITC_IRQ_PORTB, ITC_PRIORITYLEVEL_0);
-       EXTI_DeInit();
-       EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOB, EXTI_SENSITIVITY_FALL_ONLY);
-       EXTI_SetTLISensitivity(EXTI_TLISENSITIVITY_FALL_ONLY);
-       rim();
-}*/
-void clock_setup(void)
-{
-  	CLK_DeInit();
-
-  /* High speed internal clock prescaler: 1 */
-  CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_1);
-
-  /* Select HSE as system clock source */
-  CLK_SYSCLKSourceSwitchCmd(ENABLE);
-  CLK_SYSCLKSourceConfig(CLK_SYSCLKSource_HSI);
-
-  while (CLK_GetSYSCLKSource() != CLK_SYSCLKSource_HSI)
-  {}
-
-  /* Enable TIM2 CLK */
-  CLK_PeripheralClockConfig(CLK_Peripheral_TIM2, ENABLE);
-       CLK_DeInit();
-       CLK_LSICmd(DISABLE);
-       CLK_HSICmd(ENABLE);
-       while(CLK_GetFlagStatus(CLK_FLAG_HSIRDY) == FALSE);
-       CLK_SYSCLKSourceSwitchCmd(ENABLE);
-       CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_1);
-       CLK_PeripheralClockConfig(CLK_Peripheral_SPI1, ENABLE);
-       CLK_PeripheralClockConfig(CLK_Peripheral_I2C1, ENABLE);
-       CLK_PeripheralClockConfig(CLK_Peripheral_ADC1, ENABLE);
-       CLK_PeripheralClockConfig(CLK_Peripheral_USART1, ENABLE);
-       CLK_PeripheralClockConfig(CLK_Peripheral_TIM1, ENABLE);
-       CLK_PeripheralClockConfig(CLK_Peripheral_TIM2, ENABLE);
-       CLK_PeripheralClockConfig(CLK_Peripheral_TIM3, ENABLE);
 }
